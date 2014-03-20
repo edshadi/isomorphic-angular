64344d4e8246444760cb4a96c1bb2ed6b361345e
Ed Shadi <edshadi@gmail.com>
2014-03-20 10:58:58 -0700

    render index.html on initial page load

--- server/lib/express/index.js
+++ server/lib/express/index.js
@@ -1,10 +1,12 @@
 var application_root = __dirname,
     express = require('express'),
     path = require('path'),
+    cons = require('consolidate'),
     // config = require('../configuration'),
     // routes = require('../routes'),
     app = express();
 
+app.engine('html', cons.handlebars);
 app.set('view engine', 'html');
 app.set('views', 'views');
 app.set('port', 3000);
@@ -12,7 +14,7 @@ app.use(express.logger());
 app.use( express.bodyParser() );
 app.use( express.static( path.join( application_root, '../../../client/src/app/') ) );
 app.get('/', function(req, res) {
-  res.send("Isomorphic Angular");
+  res.render("index");
 });
 
 app.listen(app.get('port'), function() {
